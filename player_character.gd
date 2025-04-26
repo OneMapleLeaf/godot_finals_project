700b939c2368b5a0faaa805aa99747009b66398d
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2025-04-26 23:16:21 +0800

    returned character speed to original value

--- player_character.gd
+++ player_character.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 200
 var isWalking
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
