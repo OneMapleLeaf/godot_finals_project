ef1b097610bbb68c29bb1b44b0b73f0e950bde86
moreall <moraljoshua28@gmail.com>
2025-04-27 12:29:50 +0800

    Added 2nd, 3rd, 4th and 5th world inside the asset folder

---
+++ assets/player_character.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var speed = 200
+var isWalking
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	isWalking = false
+	if Input.is_action_pressed("W"):
+		$AnimatedSprite2D.play("walk_up")
+		isWalking = true
+	if Input.is_action_pressed("S"):
+		$AnimatedSprite2D.play("walk_down")
+		isWalking = true
+	if Input.is_action_pressed("A"):
+		$AnimatedSprite2D.play("walk_left")
+		isWalking = true
+	if Input.is_action_pressed("D"):
+		$AnimatedSprite2D.play("walk_right")
+		isWalking = true
+	if !isWalking:
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.frame = 1
+	velocity = (getInput().normalized() * speed)
+	move_and_slide()
+	
+func getInput():
+	var move = Vector2(Input.get_axis("A", "D"), Input.get_axis("W", "S"))
+	return move
