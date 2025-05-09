2b437e9d78578c0633fc844c7332a8f2c4d9fdb9
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2025-05-09 14:21:53 +0800

    changed play character movement logic

--- player_character.gd
+++ player_character.gd
@@ -5,25 +5,28 @@ var isWalking
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	isWalking = false
-	if Input.is_action_pressed("W"):
-		$AnimatedSprite2D.play("male_walk_up")
-		isWalking = true
-	if Input.is_action_pressed("S"):
-		$AnimatedSprite2D.play("male_walk_down")
-		isWalking = true
-	if Input.is_action_pressed("A"):
-		$AnimatedSprite2D.play("male_walk_left")
-		isWalking = true
+
+	# Priority: D > A > W > S
 	if Input.is_action_pressed("D"):
 		$AnimatedSprite2D.play("male_walk_right")
 		isWalking = true
-	if !isWalking:
+	elif Input.is_action_pressed("A"):
+		$AnimatedSprite2D.play("male_walk_left")
+		isWalking = true
+	elif Input.is_action_pressed("W"):
+		$AnimatedSprite2D.play("male_walk_up")
+		isWalking = true
+	elif Input.is_action_pressed("S"):
+		$AnimatedSprite2D.play("male_walk_down")
+		isWalking = true
+	else:
 		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.frame = 1
-	velocity = (getInput().normalized() * speed)
+
+	velocity = getInput().normalized() * speed
 	move_and_slide()
-	
 	isHome()
+
 func getInput():
 	var move = Vector2(Input.get_axis("A", "D"), Input.get_axis("W", "S"))
 	return move
