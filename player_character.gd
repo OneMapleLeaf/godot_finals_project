1e800bdcaedd506f3908351bd151b0c3a7d652c1
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2025-05-09 15:51:38 +0800

    added collectibles logic for player

--- player_character.gd
+++ player_character.gd
@@ -43,3 +43,14 @@ func isHome():
 		$Camera2D.limit_right = 1923
 		$Camera2D.limit_bottom = 1074
 		$Camera2D.limit_top = -2
+
+func rewardLogic():
+	
+	pass
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	var parent = area.get_parent()
+	if parent.is_in_group("collectibles"):
+		parent.queue_free()
+		
