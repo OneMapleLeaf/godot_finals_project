512f42bb3c778538d5dd887f8d971d168e69c6e5
Decimmm <rommeryumang@gmail.com>
2025-05-10 21:13:19 +0800

    character select done, sequence working intro all the way to planet xerdia

--- entername.gd
+++ entername.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+var femaleselected = false
+var maleselected = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,3 +14,20 @@ func _process(delta: float) -> void:
 
 func _on_touchscreenstart_pressed() -> void:
 	get_tree().change_scene_to_file("res://selectCharacter.tscn")
+
+
+func _on_touchscreenmale_pressed() -> void:
+	if $selectionfemale.visible == false:
+		$selectionmale.visible = true
+		
+	if $selectionfemale.visible == true:
+		$selectionmale.visible = true
+		$selectionfemale.visible = false
+
+
+func _on_touchscreenfemale_pressed() -> void:
+	if $selectionmale.visible == false:
+		$selectionfemale.visible = true
+	if $selectionmale.visible == true:
+		$selectionfemale.visible = true
+		$selectionmale.visible = false
