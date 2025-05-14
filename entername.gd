24b4828934e0beda7e6d21b21c6f35f13a67d4c7
Decimmm <rommeryumang@gmail.com>
2025-05-14 23:29:51 +0800

    many accomplishments

--- entername.gd
+++ entername.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var femaleselected = false
 var maleselected = false
+var gender
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,10 +14,13 @@ func _process(delta: float) -> void:
 
 
 func _on_touchscreenstart_pressed() -> void:
-	get_tree().change_scene_to_file("res://selectCharacter.tscn")
+	get_tree().change_scene_to_file("res://spaceship.tscn")
 
 
 func _on_touchscreenmale_pressed() -> void:
+	Entername.gender = 1
+	Globals.character_gender = "male"
+	print("male")
 	if $selectionfemale.visible == false:
 		$selectionmale.visible = true
 		
@@ -26,6 +30,9 @@ func _on_touchscreenmale_pressed() -> void:
 
 
 func _on_touchscreenfemale_pressed() -> void:
+	Entername.gender = 2
+	Globals.character_gender = "female"
+	print("female")
 	if $selectionmale.visible == false:
 		$selectionfemale.visible = true
 	if $selectionmale.visible == true:
--- resume.gd
+++ resume.gd
@@ -1,7 +1,6 @@
-extends Node
+extends Sprite2D
 
-var playerScore = 0
-var character_gender
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
