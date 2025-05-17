07dcc2cd17ffeac3162170a50b272559cf7f61e9
Decimmm <rommeryumang@gmail.com>
2025-05-17 17:40:32 +0800

    all worlds map label set

--- map_obtained.gd
+++ map_obtained.gd
@@ -11,6 +11,18 @@ func _process(delta: float) -> void:
 	if get_tree().current_scene.scene_file_path == "res://xeridia.tscn":
 		$map1.visible = true
 		$AnimatedSprite2D.play("map1")
+	if get_tree().current_scene.scene_file_path == "res://Nexar.tscn":
+		$map2.visible = true
+		$AnimatedSprite2D.play("map2")
+	if get_tree().current_scene.scene_file_path == "res://valtania.tscn":
+		$map3.visible = true
+		$AnimatedSprite2D.play("map3")
+	if get_tree().current_scene.scene_file_path == "res://Astria.tscn":
+		$map4.visible = true
+		$AnimatedSprite2D.play("map4")
+	if get_tree().current_scene.scene_file_path == "res://ZORVATH.tscn":
+		$map5.visible = true
+		$AnimatedSprite2D.play("map5")
 
 
 func _on_touchscreenbuttonx_pressed() -> void:
