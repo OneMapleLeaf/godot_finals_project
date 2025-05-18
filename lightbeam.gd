6880252a7f18e4e837f69fc8fddf691dbe3339b7
Decimmm <rommeryumang@gmail.com>
2025-05-18 20:15:02 +0800

    charselect & username validation + extraction

---
+++ lightbeam.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$AnimatedSprite2D.play("blue")
+	$AnimatedSprite2D.stop()
+	$AnimatedSprite2D.frame = 5
