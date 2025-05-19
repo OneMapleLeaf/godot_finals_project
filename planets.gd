ea88d435f994f672c9e596a0757f03eabc555afd
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2025-05-20 00:58:56 +0800

    fixed assets loading when selecting planets

--- planets.gd
+++ planets.gd
@@ -2,8 +2,18 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time
+var SPACESHIP_SCENE = SceneCach.spaceship_instance
+var XERIDIA_SCENE = SceneCach.xeridia_instance
+var NEXAR_SCENE = SceneCach.nexar_instance
+var VALTANIA_SCENE = SceneCach.valtania_instance
+var ASTRIA_SCENE = SceneCach.astria_instance
+var ZORVATH_SCENE = SceneCach.zorvath_instance
 
-
+func _ready() -> void:
+	pass
+func _process(delta: float) -> void:
+	pass
+	
 func _on_area_2_dxerdia_mouse_entered() -> void:
 	$xerdia.scale = Vector2(2.349, 2.349)
 
@@ -18,7 +28,7 @@ func _on_area_2_dnexar_mouse_entered() -> void:
 	$nexar.scale = Vector2(1.9, 1.9)
 	#testing for unlock planet
 	$nexar.modulate = Color("FFFFFF")
-	$Redfutuisticlock.visible = false
+	$Redfutuisticlock1.visible = false
 	#testing for unlock planet
 
 
@@ -53,9 +63,33 @@ func _on_area_2_dzorvath_mouse_exited() -> void:
 	$zorvath.scale = Vector2(1.696, 1.696)
 
 
-func _on_touch_screen_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://xeridia.tscn")
 
 
 func _on_backbutton_pressed() -> void:
-	get_tree().change_scene_to_file("res://spaceship.tscn")
+	redirect(SPACESHIP_SCENE)
+	
+func _on_xeridia_button_pressed() -> void:
+	if !Globals.maps[0]:
+		redirect(XERIDIA_SCENE)
+
+func _on_nexar_button_pressed() -> void:
+	if !Globals.maps[1]:
+		redirect(NEXAR_SCENE)
+
+func _on_valtania_button_pressed() -> void:
+	if !Globals.maps[2]:
+		redirect(VALTANIA_SCENE)
+
+func _on_astria_button_pressed() -> void:
+	if !Globals.maps[3]:
+		redirect(ASTRIA_SCENE)
+
+func _on_zorvath_button_pressed() -> void:
+	if !Globals.maps[4]:
+		redirect(ZORVATH_SCENE)
+
+	
+func redirect(scene: PackedScene):
+	get_tree().change_scene_to_packed(scene)
+	
+	
